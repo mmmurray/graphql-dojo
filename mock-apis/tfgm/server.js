b2c19d51e9702f7d15a7797cd749afbc849cd813
Mark Murray <mark.murray01@bbc.co.uk>
2018-04-30 07:43:03 +0100

    Mock TFGM api

---
+++ mock-apis/tfgm/server.js
@@ -0,0 +1,16 @@
+const { readFileSync } = require('fs');
+const { join } = require('path');
+
+const express = require('express');
+const app = express();
+
+const loadData = name =>
+  JSON.parse(readFileSync(join(__dirname, 'data', name), 'utf-8'));
+
+app.get('/odata/Metrolinks', (req, res) =>
+  res.json(loadData('2018-04-30_07:31:29.json'))
+);
+
+app.listen(3002, () =>
+  console.log('Mock TFGM API running: http://localhost:3002/odata/Metrolinks')
+);
