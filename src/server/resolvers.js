9130e0c919547e73e61d398c2d5e80a147d0df1d
Mark Murray <mark.murray01@bbc.co.uk>
2018-04-30 08:34:49 +0100

    trams resolver

--- src/server/resolvers.js
+++ src/server/resolvers.js
@@ -1,18 +1,27 @@
-// Some fake data
-const books = [
-  {
-    title: "Harry Potter and the Sorcerer's stone",
-    author: 'J.K. Rowling'
-  },
-  {
-    title: 'Jurassic Park',
-    author: 'Michael Crichton'
-  }
-];
+import fetch from 'node-fetch';
+
+const fetchTramData = () =>
+  fetch('http://localhost:3002/odata/Metrolinks').then(res => res.json());
+
+const createCarriages = tram =>
+  [tram.Carriages0, tram.Carriages1, tram.Carriages2, tram.Carriages3]
+    .filter(Boolean)
+    .map(size => ({ size }));
+
+const createTram = tram => ({
+  id: tram.Id,
+  line: tram.Line,
+  direction: tram.Direction,
+  station: tram.StationLocation,
+  carriages: createCarriages(tram)
+});
 
 const resolvers = {
   Query: {
-    books: () => books
+    trams: async () => {
+      const tramData = await fetchTramData();
+      return tramData.value.map(createTram);
+    }
   }
 };
 
