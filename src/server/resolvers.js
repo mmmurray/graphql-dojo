03c685b045d854ba41fce198527eab8d3a9cad20
Mark Murray <mark.murray01@bbc.co.uk>
2018-05-10 21:27:51 +0100

    ex2

--- src/server/resolvers.js
+++ src/server/resolvers.js
@@ -1,21 +1,5 @@
-import { fetchTramData } from './fetchers';
-import transformTramData from './helpers/transform-tram-data';
-
-const getStations = async () => transformTramData(await fetchTramData());
-
-const getStation = async name => (await getStations())[name];
-
 const resolvers = {
-  Query: {
-    stations: async () => Object.keys(await getStations()).map(name => ({ name })),
-    station: (root, { name }) => ({ name }),
-  },
-  Station: {
-    trams: async root => {
-      const station = await getStation(root.name);
-      return station ? station.trams : [];
-    },
-  },
+  Query: {},
 };
 
 export default resolvers;
